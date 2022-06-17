47feba547f9962aff27c98c3424e5a5330e5fa75
Umar Elmahedi <61920850+umarelmahedi@users.noreply.github.com>
2022-06-17 04:13:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,31 +2,24 @@
 echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>';
 ?>
-
 <!doctype html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.98.0">
     <title>Blaster ORG</title>
-
+    
+    <link rel="stylesheet" href="https://blaster.org.uk/css/blaster/index.css">
     <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/jumbotron/">
     <link href="/docs/5.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
     <!-- Favicons -->
-    <link rel="apple-touch-icon" href="/docs/5.2/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-    <link rel="icon" href="/docs/5.2/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-    <link rel="icon" href="/docs/5.2/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-    <link rel="manifest" href="/docs/5.2/assets/img/favicons/manifest.json">
-    <link rel="mask-icon" href="/docs/5.2/assets/img/favicons/safari-pinned-tab.svg" color="#712cf9">
-    <link rel="icon" href="/docs/5.2/assets/img/favicons/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="https://blaster.org.uk/fav/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://blaster.org.uk/fav/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://blaster.org.uk/fav/favicon-16x16.png">
+    <link rel="manifest" href="fav/site.webmanifest">
     <meta name="theme-color" content="#712cf9">
-
-
     <style>
         .bd-placeholder-img {
             font-size: 1.125rem;
@@ -79,26 +72,24 @@ echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bo
             -webkit-overflow-scrolling: touch;
         }
     </style>
-
-
 </head>
-
 <body>
-
     <main>
         <div class="container py-4">
             <header class="pb-3 mb-4 border-bottom">
                 <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
                     <img class="mb-2" src="logooo.png" alt="" width="100" height="100">
-                    <span class="fs-4">BLASTER ORGANIZATION</span>
+                    <span class="fs-4 text-success"><h1>BLASTER ORGANIZATION</h1></span>
                 </a>
             </header>
 
-            <div class="p-5 mb-4 bg-light rounded-3">
-                <div class="container-fluid py-5">
-                    <h1 class="display-5 fw-bold text-uppercase">dev server</h1>
-                    <p class="col-md-8 fs-4">This is a devoplpment server, cloned from the organization production repository on github.</p>
+            <div class="p-5 mb-4 rounded-3">
+                <div class="container-fluid py-5 hover-2">
+                    <div class="m-4 roundedd-3">
+                    <h1 class="display-5 fw-bold text-uppercase blaster-text2">dev server</h1>
+                    <p class="col-md-8 fs-4 text-success">This repository cloned from the organization account on github.</p>
                     <a href="https://github.com/umarelmahedi/deploy" target="_blank"><button class="btn btn-success btn-lg" type="button">Github</button></a>
+                    </div>
                 </div>
             </div>
             <footer class="pt-3 mt-4 text-muted border-top">
@@ -106,9 +97,5 @@ echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bo
             </footer>
         </div>
     </main>
-
-
-
 </body>
-
 </html>
